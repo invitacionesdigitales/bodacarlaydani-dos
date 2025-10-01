627fe28485a2c6e4469b6ae0aef618d09e23a31e
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2025-10-01 13:24:38 -0300

    uno

---
+++ main.js
@@ -0,0 +1,92 @@
+
+let diaEvento = new Date('December 13, 2025 21:30:00').getTime();
+let boton = document.querySelector("#btn")
+
+
+
+// diaEvento = new Date().getTime() + 1_000 * 20;
+
+let relojE = document.getElementById('reloj');
+
+const diaM = 86_400_000;
+const horaM = 3_600_000;
+const minutoM = 60_000;
+const segundoM = 1_000;
+const ejecutarReloj = setInterval(
+    () => {
+
+        let hoy = new Date().getTime();
+        let distancia = diaEvento - hoy;
+
+        let diaR = Math.trunc(distancia / diaM);
+
+        let horaR = Math.trunc((distancia % diaM) / horaM);
+
+        let minutoR = Math.trunc((distancia % horaM) / minutoM);
+
+        let segundoR = Math.trunc((distancia % minutoM) / segundoM);
+
+        if (distancia  < 1) {
+            clearInterval(ejecutarReloj);
+            diaEventoAhora();
+        }
+
+        pintarElementos(diaR, horaR, minutoR, segundoR);
+
+    }
+, 1000);
+
+let creDia = document.getElementById('cre-dia');
+let creHora = document.getElementById('cre-hora');
+let creMinuto = document.getElementById('cre-minuto');
+let creSegundo = document.getElementById('cre-segundo');
+function pintarElementos(dia, hora, minuto, segundo) {
+
+    segundo = (segundo < 10) ? '0' + segundo : segundo;
+
+    minuto = (minuto < 10) ? '0' + minuto : minuto;
+
+    hora = (hora < 10) ? '0' + hora : hora;
+
+    creDia.innerHTML = dia;
+    creHora.innerHTML = hora;
+    creMinuto.innerHTML = minuto;
+    creSegundo.innerHTML = segundo;
+}
+
+function diaEventoAhora () {
+    alert('Dia del Evento!!!');
+    // Todo el codigo cuando el evento llegue
+}
+
+
+
+
+
+
+  boton.addEventListener("click", () => {
+
+    Swal.fire({
+      title: '<strong>VER LA UBICACION<u></u></strong>',
+      icon: 'success',
+      html:
+        'INGRESÁ' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.1187751447096!2d-60.65709150000001!3d-32.9478734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7ab41fe3584a5%3A0x17f52e6964f7198!2sBas%C3%ADlica%20%22Nuestra%20Se%C3%B1ora%20de%20Lourdes%22!5e0!3m2!1ses-419!2sar!4v1759269318145!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        'Podes ampliar el mapa',
+        
+        
+    })
+  });
+  
+  function exito(){
+    Swal.fire({
+        title: '<strong>VER LA UBICACION<u></u></strong>',
+        icon: 'success',
+        html:
+          'INGRESÁ' +
+          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3352.033397104755!2d-60.717679800000006!3d-32.844367399999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b65176afaf24db%3A0xdaeeed3debe36d81!2sCampus%20Greens!5e0!3m2!1ses-419!2sar!4v1759269420462!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+          'Podes ampliar el mapa',
+          
+          
+      })
+}
